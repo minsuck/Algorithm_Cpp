d173dd2319a96617efc11b9bb93c23b8c0c069e4
036ab@naver.com <31887989+minsuck@users.noreply.github.com>
2023-01-17 11:58:15 +0900

    에라토스테네스의 채 적용했으나 틀림

--- Beakjoon/소수구하기_1929.cpp
+++ Beakjoon/소수구하기_1929.cpp
@@ -5,10 +5,11 @@ M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int getDem(int num);
+void getDem(int m, int n);
 
 int main(int argc, char* argv[]) {
     int M = 0;
@@ -16,18 +17,37 @@ int main(int argc, char* argv[]) {
 
     cin >> M >> N;
 
-    
-    vector<int> numbers(N-M+1, 0);
+    getDem(M, N);
+    //vector<int> numbers(N-M+1, 0);
 
-    for(int i=M; i<=N; i++){
-        if(getDem(i) == 1){
-            cout << i << '\n';  // \n이 endl보다 처리 속도가 훨씬 빠르다.
-        }
-    }
+    // for(int i=M; i<=N; i++){
+    //     if(getDem(M, N) == 1){
+    //         cout << i << '\n';  // \n이 endl보다 처리 속도가 훨씬 빠르다.
+    //     }
+    // }
 
 	return 0;
 }
 // 수가 1,000,000 정도로 애매하게 클 때는 에라토스테네스의 채 알고리즘 사용
-int getDem(int num){
-
+void getDem(int m, int n){
+    // 1. 2~N까지의 자연수 나열
+    vector<bool> num(1000000, true);
+    num[0] = false;
+    num[1] = false;
+    // 2. 남은 수 중 가장 작은 i 찾기
+    for(int i=0; i<sqrt(n)+1; i++){
+        if(num[i] == true){
+            int j = 2;
+            // 3. 남은 수 중 i의 배수 모두 제거
+            while(i*j < n){
+                num[i*j] = false;
+                j++;
+            }
+        }
+    }
+    for(int q=m; q<n; q++){
+        if(num[q] == true){
+            cout << q << "\n";
+        }
+    }
 }
